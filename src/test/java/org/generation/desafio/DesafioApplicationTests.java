f853094e704addf2ae7a51cd039cc7efad328b89
Thiago Filadelfo <trfiladelfo@pop-os.localdomain>
2020-12-03 00:46:30 -0300

    feat: inicio de projeto

---
+++ src/test/java/org/generation/desafio/DesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
